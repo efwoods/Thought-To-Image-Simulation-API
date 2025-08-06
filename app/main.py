c79fdf732b9cd3807717dfa34547e02c56f3a655
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-08-05 23:21:55 -0400

    updating for deployment

--- app/main.py
+++ app/main.py
@@ -36,6 +36,8 @@ import requests
 # ============================
 # Accepts image, encodes it, decodes to waveform, sends waveform_latent to Relay API
 
+# This is a new comment for deployment.
+
 # app/main.py
 import asyncio
 import websockets

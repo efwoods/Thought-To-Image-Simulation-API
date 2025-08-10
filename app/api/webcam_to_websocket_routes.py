e84d38592d3e68dfa6c294954ca84b86867e2d38
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-08-10 15:47:31 -0400

    refactored enable-thought-to-image ws route to use a random locally accessible image

--- app/api/webcam_to_websocket_routes.py
+++ app/api/webcam_to_websocket_routes.py
@@ -26,9 +26,7 @@ from core.logging import logger
 from core.config import settings
 
 from contextlib import asynccontextmanager
-
 import requests
-
 from PIL import Image
 from io import BytesIO
 import base64
@@ -41,18 +39,13 @@ from service.webcam_to_websocket_service import (
     simulate_all_images,
 )
 import pickle
-
 import os
+from glob import glob
+import random
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-IMAGE_PATHS_FLAT_LIST = os.path.join(BASE_DIR, "data", "image_paths_dict_flat_list.pkl")
-TEST_INDICES = os.path.join(BASE_DIR, "data", "test_dataset_metadata.pkl")
+TEST_STIMULUS_IMAGE_DIR = "/app/data/test_stimulus_images/"
 
-# IMAGE_PATHS_FLAT_LIST = "../data/image_paths_dict_flat_list.pkl"
-# TEST_INDICES = test_dataset_dir = "../data/test_dataset_metadata.pkl"
-
-simulation_image_index_test_full_pipeline = 0
-process_thought_to_image_index = 0
 router = APIRouter()
 
 
@@ -66,26 +59,16 @@ async def process_thought_to_image(payload: SimulationRequest):
     global process_thought_to_image_index
     timestamp = datetime.datetime.utcnow().isoformat() + "Z"
 
-    with open(IMAGE_PATHS_FLAT_LIST, "rb") as f:
-        sample_stimulus_image_path_list = pickle.load(f)
+    logger.info(f"CWD: {os.getcwd()}")
+    logger.info(f"BASE_DIR: {BASE_DIR}")
+    logger.info(f"TEST_INDICES: {TEST_STIMULUS_IMAGE_DIR}")
 
-    with open(TEST_INDICES, "rb") as f:
-        sample_stimulus_image_test_indices_list = pickle.load(f)
+    stimulus_images_l = glob(TEST_STIMULUS_IMAGE_DIR + "*.jpeg")
 
-    processing_image_path = sample_stimulus_image_path_list[
-        sample_stimulus_image_test_indices_list["indices"][
-            process_thought_to_image_index
-        ]
-    ]
+    random_stimulus_image_path = random.choice(stimulus_images_l)
+    random_stimulus_image_name = os.path.basename(random_stimulus_image_path)
 
-    processing_image_name = os.path.basename(processing_image_path)
-
-    logger.info(f"process_thought_to_image_index: {process_thought_to_image_index}")
-
-    logger.info(f"processing_image_path:{processing_image_path}")
-    logger.info(f"processing_image_name: {processing_image_name}")
-
-    image_base64 = encode_image_to_base64(processing_image_path)
+    image_base64 = encode_image_to_base64(random_stimulus_image_path)
 
     message = {
         "payload": {
@@ -95,10 +78,9 @@ async def process_thought_to_image(payload: SimulationRequest):
             "type": "test",
             "initial_timestamp": timestamp,
             "origin": "webcam-to-websocket-simulation",
-            "image_type": "PNG",
-            "processing_image_path": processing_image_path,
-            "processing_image_name": processing_image_name,
-            "process_thought_to_image_index": process_thought_to_image_index,
+            "image_type": "jpeg",
+            "processing_image_path": random_stimulus_image_path,
+            "processing_image_name": random_stimulus_image_name,
             "user_id": payload.user_id,
             "avatar_id": payload.avatar_id,
         },
@@ -121,61 +103,3 @@ async def process_thought_to_image(payload: SimulationRequest):
     except Exception as e:
         logger.error(f"[ERROR] {timestamp}: {e}")
         return json.dumps({"error": str(e)})
-
-
-@router.post("/test/full-pipeline")
-async def test_pipeline(payload: SimulationRequest):
-    global simulation_image_index_test_full_pipeline
-    timestamp = datetime.datetime.utcnow().isoformat() + "Z"
-    message = {
-        "type": "test",
-        "timestamp": timestamp,
-        "origin": "webcam-to-websocket-simulation",
-        "simulation_image_index": simulation_image_index_test_full_pipeline,
-        "payload": payload.json(),
-    }
-    logger.info(
-        f"settings.WS_ROOT_URI + /simulate/ws/test: {settings.WS_ROOT_URI}"
-        + "/simulate/ws/test"
-    )
-    logger.info(f"simulation_image_index: {simulation_image_index_test_full_pipeline}")
-
-    try:
-        async with websockets.connect(
-            settings.WS_ROOT_URI + "/simulate/ws/test"
-        ) as websocket:
-            await websocket.send(json.dumps(message))
-            response = await websocket.recv()
-            logger.info(f"[{timestamp}] Response: {response}")
-            simulation_image_index += 1
-            return response
-    except Exception as e:
-        logger.error(f"[ERROR] {timestamp}: {e}")
-        return json.dumps({"error": str(e)})
-
-
-@router.post("/simulate-test-images")
-async def simulate_test_images(payload: SimulationRequest):
-    # Simulate Test Images
-    return {
-        "response": "success",
-        "description": "This accepts an image and returns the synthetic waveform, the synthetic waveform image, and the reconstructed image.",
-    }
-
-
-@router.post("/simulate-webcam-stream")
-async def simulate_test_images(payload: SimulationRequest):
-    # Simulate Webcam Images
-    return {
-        "response": "success",
-        "description": "this will enable accept a webcame stream of images and return the associated synthetic neural waveform and the reconstructed image.",
-    }
-
-
-@router.post("/simulate-test-waveform")
-async def simulate_test_images(payload: SimulationRequest):
-    # Simulate Test Waveform Logic
-    return {
-        "response": "success",
-        "description": "This will accept a synthetic or raw neural waveform and return the reconstructed image.",
-    }
